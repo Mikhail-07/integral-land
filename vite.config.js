42ec24c54344d2c13898ace1782a477451ce8bf4
faette <lma6490544@gmail.com>
2025-11-16 19:36:46 +0300

    ghpages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import { imagetools } from "vite-imagetools"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/integral-landing/",
+  base: "/integral-land",
   plugins: [lqip(), imagetools(), react(), tailwindcss()],
 })
